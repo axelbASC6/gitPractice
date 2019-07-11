a440016bd8b963663cfe53ff01d64490d5436956
axelbASC6 <bolyaxel@gmail.com>
2019-07-11 14:47:33 -0400

    Create for loop to multiply array values

---
+++ gitPractice.js
@@ -0,0 +1,7 @@
+//function that multiplies all values in array by 2 and prints the new array
+function multipyArrayNumbers(arr){
+   
+   if(arr != " "){ for(let i=0; i < arr.lentgh; i++){
+arr[i]*=2
+    }   
+}
